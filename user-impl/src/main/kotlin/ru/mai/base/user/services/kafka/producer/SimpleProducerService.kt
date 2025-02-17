20ab9768692f3018b7399d2e8a743e64995fd6cf
Batoyan.RL <Batoyan.RL@gazprom-neft.ru>
2025-02-17 10:27:26 +0300

    Убрано лишнее логирование в продюсере

--- user-impl/src/main/kotlin/ru/mai/base/user/services/kafka/producer/SimpleProducerService.kt
+++ user-impl/src/main/kotlin/ru/mai/base/user/services/kafka/producer/SimpleProducerService.kt
@@ -19,27 +19,17 @@ class SimpleProducerService(
         completableFut
             .thenAccept { result ->
                 logger.info("Message sent to $TOPIC_NAME successfully: $result")
-                saveMessageInfo(obj, result)
             }
             .exceptionally { throwable ->
                 logger.error("Error sending message to $TOPIC_NAME", throwable)
-                handleSendError(obj, throwable)
+                handleSendSimpleJsonError(obj, throwable)
                 null
             }
 
         return "Message sending initiated for $TOPIC_NAME"
     }
 
-    private fun saveMessageInfo(message: SimpleJsonRequest, result: Any) {
-        // Реализуйте сохранение информации о сообщении (например, в базу данных или локальный файл)
-        // Пример:
-        // messageRepository.save(MessageEntity(messageId = message.id, status = "SENT", metadata = result.toString()))
-        logger.info("Message info saved: $message, result: $result")
-    }
-
-    // Заглушка для обработки ошибок отправки сообщения
-    private fun handleSendError(message: SimpleJsonRequest, throwable: Throwable) {
-        // Реализуйте логику обработки ошибки (например, логирование в БД, повторная отправка и т.д.)
+    private fun handleSendSimpleJsonError(message: SimpleJsonRequest, throwable: Throwable) {
         logger.error("Failed to send message: $message", throwable)
     }
 
